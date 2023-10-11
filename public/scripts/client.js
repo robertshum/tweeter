5ff5fa059dd825375f7e850190659c3de229d224
Robert Shum <robertshum@gmail.com>
2023-10-10 20:28:20 -0700

    added ajax to handle form submission, new methods to render tweet HTML from client.js.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,110 @@
-/*
+$(document).ready(function() {
+  /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+  //Sample Tweet:
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+
+  // Create a new tweet in the DOM
+  const createTweetElement = function(tweet) {
+
+    //creates html element(s) with jQuery.  Populates the data from a tweet.
+    const $tweet = $(`
+      <!-- an old tweet -->
+      <article class="tweet">
+        <header>
+          <!-- avatar, name and handle goes here -->
+          <div class="avatar-handle-container">
+            <div class="avatar-container">
+              <img src="${tweet.user.avatars}" alt="My lovely avatar.">
+              <h1>${tweet.user.name}</h1>
+            </div>
+            <p>${tweet.user.handle}</p>
+          </div>
+        </header>
+
+        <!-- tweeted text (re-use css!) -->
+        <textarea class="tweet-field" name="text" readonly>${tweet.content.text}</textarea>
+
+        <footer>
+          <!-- day and icons go here -->
+          <div class="days-posted-icons-container">
+            <p>${tweet.created_at}</p>
+            <div>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </div>
+        </footer>
+      </article>
+      `);
+
+    //return this tweet.
+    return $tweet;
+  };
+
+  // Render tweets on the DOM
+  const renderTweets = function(tweetArray) {
+
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweetObj of tweetArray) {
+      const $tweet = createTweetElement(tweetObj);
+
+      //TODO check for new tweets that should show top of container.
+      $('#tweets-container').append($tweet);
+    }
+  };
+
+  //handle submit events with AJAX
+  $(".tweet-form").on("submit", function(event) {
+
+    //stop the submission from our HTML form
+    event.preventDefault();
+
+    // Serialize the form data:
+    // Looks at all the 'name' attributes in event.target (or a form in our case), and then collects the values of them.  It then encodes it and returns a obj.
+    console.log("event.target", event.target);
+    const formData = $(event.target).serialize();
+
+    //ajax post request that doesn't refreshthe page
+    $.post("/tweets", formData, function(_response) {
+      //TODO we can do something fun with the _response for success messages!
+
+    });
+  });
+
+  renderTweets(data);
+
+
+});
